23d6cc387e12ce16f559aa102c0e3001604c84f1
CIAC <CIAC@CIAC2-13.academic.mixcoac.upmx.mx>
2025-09-25 13:02:19 -0600

    CLASE 09 SEPTIEMBRE

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
 export default function Catalogo() {
+
+  const products = [
+    { nombre: "Camera", cantidad: 4, id: 1354 },
+    { nombre: "Microfono", cantidad: 4, id: 1355 },
+    { nombre: "Audifonos", cantidad: 4, id: 1356 },
+    { nombre: "Television", cantidad: 4, id: 1357 },
+  ]
+
   return (
-    <div className = 'flex flex-wrap justify-center'>
-        <div className = 'shadow shadow-pink-200 border rounded-2xl w-4/9 flex justify-center'>
-        <p>MI PRIMER PRODUCTO</p>
+    <div className='flex flex-wrap justify-center'>
+      {products.map((product) => (
+        <div key = {product.id} className='border rounded-2xl w-4/12 flex justify-center font-bold bg-pink-200 border-pink-200 text-white m-2'>
+          <p>{product.nombre}</p>
         </div>
+      ))}
     </div>
+
+
   )
 }
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className = 'flex flex-wrap justify-center bg-yellow-50 text-pink-300 hover:text-pink-200'>
-        <h1>hola</h1> 
+    <div className = 'flex items-center flex-wrap justify-center bg-yellow-50 text-pink-300 hover:text-pink-200 font-extrabold h-12'>
+        <h1 className = ''>SHOPPING CART</h1> 
       
     </div>
   )
